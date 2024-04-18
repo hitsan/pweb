b52fa28b963f3d709e2efc04fdc7f93599fa7c86
hitsan <soledewa2828@gmail.com>
2024-04-18 21:52:35 +0900

    change sql.sql and make_devcontainer.sh

--- db/sql.sql
+++ db/sql.sql
@@ -1,2 +1,14 @@
-create database if not exists LESSON;
-use LESSON;
+create database workdb;
+use workdb;
+CREATE TABLE Works (
+    dt DATE NOT NULL,
+    task VARCHAR(255) NOT NULL,
+    categry_id INTEGER,
+    working_hour FLOAT,
+    progress INTEGER,
+    PRIMARY KEY (dt, task)
+);
+CREATE TABLE Categry (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    categry CHAR(8) NOT NULL
+);
--- make_devcontainer.sh
+++ make_devcontainer.sh
@@ -6,4 +6,4 @@ if [ -z "$1" ]; then
 fi
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
-docker compose build $1
+docker compose build $1 --no-cache
